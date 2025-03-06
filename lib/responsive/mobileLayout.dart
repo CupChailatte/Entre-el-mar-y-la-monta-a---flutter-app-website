e53dde7837d59077e440e59a05e3524eca5b4908
CupChailatte <Chaisoda864@gmail.com>
2025-03-06 10:43:23 +0100

    second commit

---
+++ lib/responsive/mobileLayout.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Mobilelayout extends StatelessWidget {
+  const Mobilelayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Expanded(child: Container(color: Colors.red,)),);
+  }
+}
